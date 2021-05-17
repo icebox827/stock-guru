0ae33273ca2892af9f7b60b76db619a11ddb03d7
Denis Lafontant <icebox827@gmail.com>
2021-05-17 14:04:24 -0400

    Update the stock and filter

--- src/actions/index.js
+++ src/actions/index.js
@@ -12,58 +12,45 @@ import {
   FILTER_STOCK_SUCCESS,
   FILTER_STOCK_FAILURE,
 } from './action';
-
 const fetchStock = () => async(dispatch) => {
   dispatch({ type: FETCH_STOCK_REQUEST });
-
   try {
-    const response = await fetch('https://financialmodelingprep.com/api/v3/actives?apikey=71692019ce2067a12139d5ef9415ecdb');
+    const response = await fetch('https://financialmodelingprep.com/api/v3/actives?apikey=8363e942a393eaaf6061932fbb0ad836');
     const data = await response.json();
-
     dispatch({ type: FETCH_STOCK_SUCCESS, payload: data })
   }catch (error) {
     dispatch({ type: FETCH_STOCK_FAILURE, payload: error });
   }
 };
-
 const fetchCompanies = () => async(dispatch) => {
   dispatch({ type: FETCH_COMPANIES_REQUEST });
-
   try {
-    const response = await fetch('https://financialmodelingprep.com/api/v3/stock-screener?marketCapLowerThan=10000000000000&betaMoreThan=1&volumeMoreThan=100&exchange=NYSE,NASDAQ&apikey=71692019ce2067a12139d5ef9415ecdb');
+    const response = await fetch('https://financialmodelingprep.com/api/v3/stock-screener?marketCapLowerThan=10000000000000&betaMoreThan=1&volumeMoreThan=100&exchange=NYSE,NASDAQ&apikey=8363e942a393eaaf6061932fbb0ad836');
     const data = await response.json();
-
     dispatch({ type: FETCH_COMPANIES_SUCCESS, payload: data });
   } catch (error) {
     dispatch({ type: FETCH_COMPANIES_FAILURE, payload: error});
   }
 };
-
 console.log(fetchCompanies());
-
 const fetchForex = () => async(dispatch) => {
   dispatch({ type: FETCH_FOREX_REQUEST });
-
   try {
-    const response = await fetch('https://financialmodelingprep.com/api/v3/fx?apikey=71692019ce2067a12139d5ef9415ecdb');
+    const response = await fetch('https://financialmodelingprep.com/api/v3/fx?apikey=8363e942a393eaaf6061932fbb0ad836');
     const data = await response.json();
-
     dispatch({ type: FETCH_FOREX_SUCCESS, payload: data });
   }catch (error) {
     dispatch({ type: FETCH_FOREX_FAILURE, payload: error });
   }
 };
-
-const filterStock = (stock) => async(dispatch) => {
+const filterStock = (stock="actives") => async(dispatch) => {
   dispatch({ type: FILTER_STOCK_REQUEST });
-
   try {
-    const response = await fetch(`https://financialmodelingprep.com/api/v3/${stock}?apikey=71692019ce2067a12139d5ef9415ecdb`);
+    const response = await fetch(`https://financialmodelingprep.com/api/v3/${stock}?apikey=8363e942a393eaaf6061932fbb0ad836`);
     const data = await response.json();
     dispatch({ type: FILTER_STOCK_SUCCESS, payload: data });
   }catch (error) {
     dispatch({ type: FILTER_STOCK_FAILURE, payload: error });
   }
 };
-
 export { fetchStock, fetchCompanies, fetchForex, filterStock };
--- src/containers/Stock.js
+++ src/containers/Stock.js
@@ -4,55 +4,23 @@ import { useDispatch, useSelector } from 'react-redux';
 import { fetchStock } from '../actions/index';
 import { Card } from 'react-bootstrap';
 import { filterStock } from '../actions/index';
-
 const filters = [
   'actives',
   'gainers',
   'losers'
 ];
-
 const Stock = () => {
   const dispatch = useDispatch();
-  const { stocks, loading, error } = useSelector((state) => state.stocks);
-
+  const { filter, loading, error } = useSelector((state) => state.filter);
   const selectedstocks = filters.map(filter => <option key={filter} value={filter}>{filter}</option>);
-
   const handleChange = e => {
     dispatch(filterStock(e.target.value));
   };
-
   useEffect(() => {
-    
+    dispatch(filterStock());
   },[dispatch]);
-
-  useEffect(() => {
-    dispatch(fetchStock());
-  },[dispatch]);
-
-  const renderStocks = () => {
     if (loading) return <h1>Loading data...</h1>;
     if (error) return <h1>Error try again!</h1>;
-
-    return stocks.map((stock) => (
-     <Card style={{ width: '21rem' }} className="bg-dark text-white mt-5" key={stock.ticker}>
-       <Card.Body>
-         <Card.Title className="ticker">{stock.ticker}</Card.Title>
-         <Card.Subtitle className="companyName">{stock.companyName}</Card.Subtitle>
-         <Card.Text className="stockText">
-            <span className="profit">Price: </span>
-            {stock.price}
-            {' '}
-            USD
-            <br />
-            <span className="profit">Profit: </span>
-            {' '}
-            {stock.changesPercentage}
-          </Card.Text>
-       </Card.Body>
-     </Card>
-    ));
-  };
-
   return (
     <Flex wrap="wrap" display="flex" w="100%">
       <form id="form" className="form-flex">
@@ -69,9 +37,27 @@ const Stock = () => {
           </select>
         </fieldset>
       </form>
-      {renderStocks()}
+      {
+        filter.map(stock => (
+          <Card style={{ width: '21rem' }} className="bg-dark text-white mt-5" key={stock.ticker}>
+            <Card.Body>
+              <Card.Title className="ticker">{stock.ticker}</Card.Title>
+              <Card.Subtitle className="companyName">{stock.companyName}</Card.Subtitle>
+              <Card.Text className="stockText">
+                 <span className="profit">Price: </span>
+                 {stock.price}
+                 {' '}
+                 USD
+                 <br />
+                 <span className="profit">Profit: </span>
+                 {' '}
+                 {stock.changesPercentage}
+               </Card.Text>
+            </Card.Body>
+          </Card>
+         ))
+      }
     </Flex>
   )
 };
-
 export default Stock;
