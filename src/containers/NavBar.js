66f93e4174f2f112554afa2ccfbea0f898ac9e29
Denis Lafontant <icebox827@gmail.com>
2021-05-16 21:49:25 -0400

    Style navbar

--- src/containers/NavBar.js
+++ src/containers/NavBar.js
@@ -5,18 +5,18 @@ const NavBar = () => {
   return (
     <Menu>
      <Grid templateColumns="repeat(3, 1fr)">
-      <Box bg="tomato">
-        <MenuItem>
+      <Box bg="brown">
+        <MenuItem color="white" className="link">
           <Link to="/"><h3>Stock</h3></Link>
         </MenuItem>
       </Box>
-      <Box bg="tomato">
-        <MenuItem>
+      <Box bg="brown">
+        <MenuItem color="white" className="link">
           <Link to="/company"><h3>Company</h3></Link>
         </MenuItem>
       </Box>
-      <Box bg="tomato">
-        <MenuItem>
+      <Box bg="brown">
+        <MenuItem color="white" className="link">
           <Link to="/forex"><h3>Forex</h3></Link>
         </MenuItem>
       </Box>
