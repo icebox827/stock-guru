f0b37cba371a70c71f778fcd99eedce60eded2d4
Denis Lafontant <icebox827@gmail.com>
2021-05-18 16:05:42 -0400

    Add more test

--- src/_tests_/action.test.js
+++ src/_tests_/action.test.js
@@ -10,15 +10,15 @@ describe(
         expect(action).not.toBe('');
       },
     );
-    it('Should return fetch request', () => {
+    it('Should return stock request', () => {
       const action = actions.FETCH_STOCK_REQUEST;
       expect(action).toBe('FETCH_STOCK_REQUEST');
     });
-    it('Should return fetch success', () => {
+    it('Should return stock success', () => {
       const action = actions.FETCH_STOCK_SUCCESS;
       expect(action).toBe('FETCH_STOCK_SUCCESS');
     });
-    it('Should return fetch failure', () => {
+    it('Should return stock failure', () => {
       const action = actions.FETCH_STOCK_FAILURE;
       expect(action).toBe('FETCH_STOCK_FAILURE');
     });
@@ -35,6 +35,18 @@ describe(
         expect(action).not.toBe('');
       },
     );
+    it('Should return company request', () => {
+      const action = actions.FETCH_COMPANIES_REQUEST;
+      expect(action).toBe('FETCH_COMPANIES_REQUEST');
+    });
+    it('Should return company success', () => {
+      const action = actions.FETCH_COMPANIES_SUCCESS;
+      expect(action).toBe('FETCH_COMPANIES_SUCCESS');
+    });
+    it('Should return company failure', () => {
+      const action = actions.FETCH_COMPANIES_FAILURE;
+      expect(action).toBe('FETCH_COMPANIES_FAILURE');
+    });
   },
 );
 
@@ -48,6 +60,18 @@ describe(
         expect(action).not.toBe('');
       },
     );
+    it('Should return forex request', () => {
+      const action = actions.FETCH_FOREX_REQUEST;
+      expect(action).toBe('FETCH_FOREX_REQUEST');
+    });
+    it('Should return forex success', () => {
+      const action = actions.FETCH_FOREX_SUCCESS;
+      expect(action).toBe('FETCH_FOREX_SUCCESS');
+    });
+    it('Should return forex failure', () => {
+      const action = actions.FETCH_FOREX_FAILURE;
+      expect(action).toBe('FETCH_FOREX_FAILURE');
+    });
   },
 );
 
@@ -61,5 +85,17 @@ describe(
         expect(action).not.toBe('');
       },
     );
+    it('Should return stock detail request', () => {
+      const action = actions.FETCH_STOCK_ITEM_REQUEST;
+      expect(action).toBe('FETCH_STOCK_ITEM_REQUEST');
+    });
+    it('Should return stock detail success', () => {
+      const action = actions.FETCH_STOCK_ITEM_SUCCESS;
+      expect(action).toBe('FETCH_STOCK_ITEM_SUCCESS');
+    });
+    it('Should return stock detail failure', () => {
+      const action = actions.FETCH_STOCK_ITEM_FAILURE;
+      expect(action).toBe('FETCH_STOCK_ITEM_FAILURE');
+    });
   },
 );
