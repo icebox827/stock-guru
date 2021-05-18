120bb40af9e2b9227002ec25817c5ff5098136c5
Denis Lafontant <icebox827@gmail.com>
2021-05-18 10:03:12 -0400

    Recover app

--- src/index.js
+++ src/index.js
@@ -1,27 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-<<<<<<< HEAD
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-=======
 import './styles/index.css';
 import App from './components/App';
->>>>>>> feature_app
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-<<<<<<< HEAD
-  document.getElementById('root')
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
-=======
   document.getElementById('root'),
 );
->>>>>>> feature_app
