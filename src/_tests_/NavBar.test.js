75a1d558121d8e78f23952dedbd41e06067e17b6
Denis Lafontant <icebox827@gmail.com>
2021-05-18 10:35:05 -0400

    Update eslint

--- src/_tests_/NavBar.test.js
+++ src/_tests_/NavBar.test.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import '@testing-library/jest-dom/extend-expect';
 import { MemoryRouter } from 'react-router-dom';
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import Navbar from '../components/Navbar';
 
 describe('<Navbar />', () => {
