e5cb87b186337748d7908593db3170da6b11252b
Denis Lafontant <icebox827@gmail.com>
2021-05-18 13:36:28 -0400

    Fix all linters

--- src/_tests_/action.test.js
+++ src/_tests_/action.test.js
@@ -1,29 +1,69 @@
-import * as actions from '../actions/action'
+import * as actions from "../actions/action";
 
-describe('Evaluate stockRequest', () => {
-  it('Should not be an empty object', () => {
-    const action = actions.stockRequest
-    expect(action).not.toBe('')
-  })
-})
+describe(
+    "Evaluate stockRequest",
+    () => {
 
-describe('Evaluate companyRequest', () => {
-  it('Should not be an empty object', () => {
-    const action = actions.companyRequest
-    expect(action).not.toBe('')
-  })
-})
+        it(
+            "Should not be an empty object",
+            () => {
 
-describe('Evaluate forexRequest', () => {
-  it('Should not be an empty object', () => {
-    const action = actions.forexRequest
-    expect(action).not.toBe('')
-  })
-})
+                const action = actions.stockRequest;
+                expect(action).not.toBe("");
 
-describe('Evaluate stockDetailRequest', () => {
-  it('Should not be an empty object', () => {
-    const action = actions.stockDetailRequest
-    expect(action).not.toBe('')
-  })
-})
+            }
+        );
+
+    }
+);
+
+describe(
+    "Evaluate companyRequest",
+    () => {
+
+        it(
+            "Should not be an empty object",
+            () => {
+
+                const action = actions.companyRequest;
+                expect(action).not.toBe("");
+
+            }
+        );
+
+    }
+);
+
+describe(
+    "Evaluate forexRequest",
+    () => {
+
+        it(
+            "Should not be an empty object",
+            () => {
+
+                const action = actions.forexRequest;
+                expect(action).not.toBe("");
+
+            }
+        );
+
+    }
+);
+
+describe(
+    "Evaluate stockDetailRequest",
+    () => {
+
+        it(
+            "Should not be an empty object",
+            () => {
+
+                const action = actions.stockDetailRequest;
+                expect(action).not.toBe("");
+
+            }
+        );
+
+    }
+);
