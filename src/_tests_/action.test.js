777a7a9298585fb427b2e2a648d0f75404247bb8
Denis Lafontant <icebox827@gmail.com>
2021-05-18 15:59:36 -0400

    Update stock test

--- src/_tests_/action.test.js
+++ src/_tests_/action.test.js
@@ -10,6 +10,18 @@ describe(
         expect(action).not.toBe('');
       },
     );
+    it('Should return fetch request', () => {
+      const action = actions.FETCH_STOCK_REQUEST;
+      expect(action).toBe('FETCH_STOCK_REQUEST');
+    });
+    it('Should return fetch success', () => {
+      const action = actions.FETCH_STOCK_SUCCESS;
+      expect(action).toBe('FETCH_STOCK_SUCCESS');
+    });
+    it('Should return fetch failure', () => {
+      const action = actions.FETCH_STOCK_FAILURE;
+      expect(action).toBe('FETCH_STOCK_FAILURE');
+    });
   },
 );
 
