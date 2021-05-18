2be19e1970e4b8fbe2a989d440bf8a68ce726d16
Denis Lafontant <icebox827@gmail.com>
2021-05-18 15:50:27 -0400

    Fix app lint

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
-// eslint-disable-next-line no-unused-vars
-import { ChakraProvider, chakra } from '@chakra-ui/react';
+import { ChakraProvider } from '@chakra-ui/react';
 import Routes from './Routes';
 
 function App() {
