6110ef4e78858e50e9f1af1e8764ea6918f928f2
Denis Lafontant <icebox827@gmail.com>
2021-05-20 12:17:35 -0400

    Create filter company and action filter

---
+++ src/components/CompanyFilter.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+
+const CompanyFilter = () => {
+  const filters = ['All', 'NASDAQ', 'NYSE'];
+};
+
+export default CompanyFilter;
--- src/components/StockFilter.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const filters = ['actives', 'gainers', 'losers'];
-
-const StockFilter = () => {
-
-};
-
-export default StockFilter;
