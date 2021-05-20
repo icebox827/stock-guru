3e89810415e1f5777cc0a1ce121576c927f3f749
Denis Lafontant <icebox827@gmail.com>
2021-05-20 17:56:08 -0400

    Create filterReducer test

---
+++ src/_tests_/filterReducer.test.js
@@ -0,0 +1,16 @@
+import { filterStock } from '../actions/index';
+import filterReducer from '../reducers/filterReducer';
+
+
+describe('filter reducer', () => {
+  it('Should return the default state', () => {
+    expect('').toEqual('');
+  });
+
+  it('Should return new state if a valid filter is provide', () => {
+    const action = filterStock({ filter: 'Action'});
+    const state = filterReducer(undefined, action);
+
+    expect(state).toEqual(filter)
+  });
+});
