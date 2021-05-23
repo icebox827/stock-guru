4ed8c70bdb00cec45c2bda3eecc1c4c9541d7c1b
Denis Lafontant <icebox827@gmail.com>
2021-05-22 21:13:44 -0400

    Create news container

---
+++ src/containers/News.js
@@ -0,0 +1,84 @@
+import { Flex } from '@chakra-ui/react';
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { Card } from 'react-bootstrap';
+import { fetchNews } from '../actions/index';
+
+function News () {
+  const dispatch = useDispatch();
+  const { news, loading, error } = useSelector((state) => state.news);
+
+  useEffect(
+    () => {
+      dispatch(fetchNews());
+    },
+    [dispatch]
+  );
+
+  const renderNews = () => {
+    if (loading) {
+      return (
+        <h1>
+          Loading data...
+        </h1>
+      );
+    }
+    if (error) {
+      return (
+        <h1>
+          Error try again!
+        </h1>
+      );
+    }
+
+    return news.map((nws) => {
+      <Card
+        className="bg-dark text-white mt-3"
+        key={nws.symbol}
+        style={{ width: '50rem' }}
+      >
+        <Card.Body>
+          <Card.Title className="symbol">
+            <h2>
+              {nws.symbol}
+            </h2>
+          </Card.Title>
+          <Card.Subtitle>
+            <h3>
+              {nws.title}
+            </h3>
+          </Card.Subtitle>
+          <Card.Img>
+            {nws.image}
+          </Card.Img>
+          <Card.Text>
+            <h5>
+            {nws.text}
+            </h5>
+            
+            <h6>
+              Site: 
+              {nws.site}
+              <br />
+              <a
+              className="website"
+              href={nws.url}
+              rel="noopener"
+              target="__blank"
+            >
+              {' '}
+              Visit Website
+            </a>
+            </h6>
+            <h6>
+              date:
+              {nws.publishedDate}
+            </h6>
+          </Card.Text>
+        </Card.Body>
+      </Card>
+    })
+  };
+};
+
+export default News;
