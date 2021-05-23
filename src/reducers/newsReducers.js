25bc254414019616c0fd46bc6783fd69a1533a9d
Denis Lafontant <icebox827@gmail.com>
2021-05-22 20:35:32 -0400

    Create News reducer

---
+++ src/reducers/newsReducers.js
@@ -0,0 +1,38 @@
+import {
+  FETCH_NEWS_REQUEST,
+  FETCH_NEWS_SUCCESS,
+  FETCH_NEWS_FAILURE,
+} from '../actions/action';
+
+const INITIAL_STATE = {
+  news: [],
+  loading: false,
+  error: null,
+};
+
+const newsReducers = (state = INITIAL_STATE, action) => {
+  switch(action.type) {
+    case FETCH_NEWS_REQUEST:
+      return {
+        ...state,
+        loading: true,
+        error: null,
+      };
+    case FETCH_NEWS_SUCCESS:
+      return {
+        ...state,
+        loading: false,
+        news: action.payload,
+      };
+    case FETCH_NEWS_FAILURE:
+      return {
+        ...state,
+        loading: false,
+        error: action.payload,
+      };
+    default:
+      return state;
+  }
+};
+
+export default newsReducers;
