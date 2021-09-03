972fdc51d8f5a3da4c932c020f72e1a97c16eb2d
Denis Lafontant <icebox827@gmail.com>
2021-09-03 19:56:32 -0400

    Style Navbar

--- src/containers/NavBar.js
+++ src/containers/NavBar.js
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 import { HamburgerIcon, CloseIcon, ChevronDownIcon } from '@chakra-ui/icons';
 import {
-  Flex, MenuItem, Menu, Text, Box, 
+  Flex, MenuItem, Menu, Text, Box, Spacer, Divider
 } from '@chakra-ui/react';
 import { Link } from 'react-router-dom';
 
@@ -15,6 +15,7 @@ function NavBar() {
           </Text>
         </Link>
       </Box>
+      <Spacer/>
       <Box>
         <Link to="/" className="nav-link active">
           <Text fontSize="2xl" color="white">
@@ -22,29 +23,33 @@ function NavBar() {
           </Text>
         </Link>
       </Box>
+      <Divider orientation="horizontal" />
       <Box>
-        <Link to="/stock">
+        <Link to="/stock" className="nav-link">
           <Text fontSize="2xl" color="white">
             Stock
           </Text>
         </Link>
       </Box>
+      <Divider orientation="horizontal" />
       <Box>
-        <Link to="/company">
+        <Link to="/company" className="nav-link">
           <Text fontSize="2xl" color="white">
             Company
           </Text>
         </Link>
       </Box>
+      <Divider orientation="horizontal" />
       <Box>
-        <Link to="/forex">
+        <Link to="/forex" className="nav-link">
           <Text fontSize="2xl" color="white">
             Forex
           </Text>
         </Link>
       </Box>
+      <Divider orientation="horizontal" />
       <Box>
-        <Link to="/crypto">
+        <Link to="/crypto" className="nav-link">
           <Text fontSize="2xl" color="white">
             Crypto
           </Text>
