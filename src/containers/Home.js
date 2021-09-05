232120ef0ee9af68e161bd9ccf9ab3c2ef0c6987
Denis Lafontant <icebox827@gmail.com>
2021-09-05 16:48:28 -0400

    Add buy a coffee button

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -128,6 +128,16 @@ const Home = () => {
               </Card.Body>
             )) }
           </Card>
+          <Box align="center">
+            <form action="https://www.paypal.com/donate" method="post" target="_top">
+              <input type="hidden" name="business" value="H8KJWYJNE4EF8" />
+              <input type="hidden" name="no_recurring" value="0" />
+              <input type="hidden" name="item_name" value="Buy us a coffee" />
+              <input type="hidden" name="currency_code" value="USD" />
+              <input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_donateCC_LG.gif" border="0" name="submit" title="PayPal - The safer, easier way to pay online!" alt="Donate with PayPal button" />
+              <img alt="" border="0" src="https://www.paypal.com/en_US/i/scr/pixel.gif" width="1" height="1" />
+            </form>
+          </Box>
         </GridItem>
         <GridItem
           rowSpan = {1}
