a4afc297328a879b5b0654ee9823eeeddcdc2ae9
Denis Lafontant <icebox827@gmail.com>
2021-09-09 17:15:46 -0400

    Update responisve navbar

--- src/containers/NavBar.js
+++ src/containers/NavBar.js
@@ -1,6 +1,7 @@
+import { useState } from 'react';
 import { HamburgerIcon, CloseIcon, ChevronDownIcon } from '@chakra-ui/icons';
 import {
-  Flex, MenuItem, Menu, Text, Box, Spacer, Divider
+  Flex, MenuItem, Menu, Text, Box, Spacer, Divider, IconButton
 } from '@chakra-ui/react';
 import { Link } from 'react-router-dom';
 
@@ -10,7 +11,7 @@ function NavBar() {
       classNameName="Navbar" 
       backgroundColor="teal" 
       h="3rem" 
-      w="100%"
+      w="100vw"
       pos="fixed"
       align="center"
     >
@@ -21,6 +22,59 @@ function NavBar() {
           </Text>
         </Link>
       </Box>
+      <Flex
+        w="100vw"
+        h="100vh"
+        backgroundColor="teal"
+        pos="fixed"
+        align="center"
+        top="0"
+        left="0"
+        overflowY="auto"
+        flexDir="column"
+        zIndex={20}
+      >
+        <Flex
+          flexDir="column"
+          align="center"
+        >
+          <Box>
+            <Link to="/">
+              <Text fontSize="2xl" color="white" className="nav-link">
+                Home 
+              </Text>
+            </Link>
+          </Box>
+          <Box>
+            <Link to="/stock">
+              <Text fontSize="2xl" color="white" className="nav-link">
+                Stock
+              </Text>
+            </Link>
+          </Box>
+          <Box>
+            <Link to="/company" >
+              <Text fontSize="2xl" color="white" className="nav-link">
+                Company
+              </Text>
+            </Link>
+          </Box>
+          <Box>
+            <Link to="/forex" >
+              <Text fontSize="2xl" color="white" className="nav-link">
+                Forex
+              </Text>
+            </Link>
+          </Box>
+          <Box>
+            <Link to="/crypto" >
+              <Text fontSize="2xl" color="white" className="nav-link">
+                Crypto
+              </Text>
+            </Link>
+          </Box> 
+        </Flex>
+      </Flex>
       <Flex 
         display={['none', 'none', 'flex', 'flex']}
       >
@@ -60,6 +114,15 @@ function NavBar() {
           </Link>
         </Box> 
       </Flex>
+      <Spacer />
+      <IconButton 
+        aria-label="Open Menu"
+        size="md"
+        mr={2}
+        icon={<HamburgerIcon />}
+        display={['flex', 'flex', 'none', 'none']}
+        backgroundColor="teal.100"
+      />
     </Flex>
   );
 }
