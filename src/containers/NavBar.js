96e361ea7433660613d3d0f1ff86910fdae679fb
Denis Lafontant <icebox827@gmail.com>
2021-09-09 16:47:00 -0400

    Hide nav element in small screen size

--- src/containers/NavBar.js
+++ src/containers/NavBar.js
@@ -14,49 +14,51 @@ function NavBar() {
       pos="fixed"
       align="center"
     >
-       <Box>
+      <Box>
         <Link to="/">
           <Text fontSize="2xl" color="yellow.500" className="nav-link">
             Stock Guru
           </Text>
         </Link>
       </Box>
-      <Flex>
-         <Box>
-        <Link to="/">
-          <Text fontSize="2xl" color="white" className="nav-link">
-            Home 
-          </Text>
-        </Link>
-      </Box>
-      <Box>
-        <Link to="/stock">
-          <Text fontSize="2xl" color="white" className="nav-link">
-            Stock
-          </Text>
-        </Link>
-      </Box>
-      <Box>
-        <Link to="/company" >
-          <Text fontSize="2xl" color="white" className="nav-link">
-            Company
-          </Text>
-        </Link>
-      </Box>
-      <Box>
-        <Link to="/forex" >
-          <Text fontSize="2xl" color="white" className="nav-link">
-            Forex
-          </Text>
-        </Link>
-      </Box>
-      <Box>
-        <Link to="/crypto" >
-          <Text fontSize="2xl" color="white" className="nav-link">
-            Crypto
-          </Text>
-        </Link>
-      </Box> 
+      <Flex 
+        display={['none', 'none', 'flex', 'flex']}
+      >
+        <Box>
+          <Link to="/">
+            <Text fontSize="2xl" color="white" className="nav-link">
+              Home 
+            </Text>
+          </Link>
+        </Box>
+        <Box>
+          <Link to="/stock">
+            <Text fontSize="2xl" color="white" className="nav-link">
+              Stock
+            </Text>
+          </Link>
+        </Box>
+        <Box>
+          <Link to="/company" >
+            <Text fontSize="2xl" color="white" className="nav-link">
+              Company
+            </Text>
+          </Link>
+        </Box>
+        <Box>
+          <Link to="/forex" >
+            <Text fontSize="2xl" color="white" className="nav-link">
+              Forex
+            </Text>
+          </Link>
+        </Box>
+        <Box>
+          <Link to="/crypto" >
+            <Text fontSize="2xl" color="white" className="nav-link">
+              Crypto
+            </Text>
+          </Link>
+        </Box> 
       </Flex>
     </Flex>
   );
